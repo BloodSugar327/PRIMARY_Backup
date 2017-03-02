bf36b0e78c97bf3bd852c4c28c43d006af7e74db
David Guerrero <DavidG@pla-arch01-770.local>
2017-03-02 15:31:58 -0800

    Initial Commit

---
+++ PRIMARY_Backup/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PRIMARY_Backup
+//
+//  Created by David Guerrero on 3/2/17.
+//  Copyright © 2017 Primary Color. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PRIMARY_Backup/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PRIMARY_Backup
+//
+//  Created by David Guerrero on 3/2/17.
+//  Copyright © 2017 Primary Color. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
