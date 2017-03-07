ee7502a7cef880f01b7fbc54de7a9e7237aacf8d
David Guerrero <DavidG@pla-arch01-770.local>
2017-03-07 15:55:24 -0800

    Updates

--- PRIMARY_Backup/FolderMerger.swift
+++ PRIMARY_Backup/FolderMerger.swift
@@ -5,195 +5,186 @@
 //  Created by David Guerrero on 3/3/17.
 //  Copyright © 2017 Primary Color. All rights reserved.
 //
-//d
-//
-//
+
 
 import Foundation
 import Cocoa
 
 class Folder_Merger
-{
-    
-    //Create two enumerations to control the type of movement and conflict resolution options
-    enum ActionType { case move, copy }
-    enum ConflictResolution { case keepSource, keepDestination }
-    
-    //Declare and initialize a FileManager object
-    private let fileManager = FileManager()
-    
-    
-    private var actionType: ActionType!
-    private var conflictResolution: ConflictResolution!
-    private var deleteEmptyFolders: Bool!
-    
-    
-    //Set Initialization for this class
-    init(actionType: ActionType = .move, conflictResolution: ConflictResolution = .keepDestination, deleteEmptyFolders: Bool = false)
     {
-        //set user settings from enumerations
-        self.actionType = actionType
-        self.conflictResolution = conflictResolution
-        self.deleteEmptyFolders = deleteEmptyFolders
-    }
-    
-    //Function used to merge folders using two String based paths
-    func merge(atPath: String, toPath: String)
-    {
-        //Initialize enumerator object from fileManager object
-        let pathEnumerator = fileManager.enumerator(atPath: atPath)
         
-        //String array used to store folders
-        var folders: [String] = [atPath]
+        //Create two enumerations to control the type of movement and conflict resolution options
+        enum ActionType { case move, copy }
+        enum ConflictResolution { case keepSource, keepDestination }
         
-        //Move through all the items in the folder
-        while let relativePath = pathEnumerator?.nextObject() as? String
-        {
-            //Set curremt path of the item
-            let subItemAtPath = URL(fileURLWithPath: atPath).appendingPathComponent(relativePath).path
-            //Set new path for the item
-            let subItemToPath = URL(fileURLWithPath: toPath).appendingPathComponent(relativePath).path
-            
-            //Use isDir fuction to see if the object is a directory or file
-            //If its a directory
-            if isDir(atPath: subItemAtPath)
+        //Declare and initialize a FileManager object
+        private let fileManager = FileManager()
+        
+        
+        private var actionType: ActionType!
+        private var conflictResolution: ConflictResolution!
+        private var deleteEmptyFolders: Bool!
+        
+        
+        //Set Initialization for this class
+        init(actionType: ActionType = .move, conflictResolution: ConflictResolution = .keepDestination, deleteEmptyFolders: Bool = false)
             {
+                //set user settings from enumerations
+                self.actionType = actionType
+                self.conflictResolution = conflictResolution
+                self.deleteEmptyFolders = deleteEmptyFolders
+            }
+        
+        //Function used to merge folders using two String based paths
+        func merge(atPath: String, toPath: String)
+            {
+                //Initialize enumerator object from fileManager object
+                let pathEnumerator = fileManager.enumerator(atPath: atPath)
                 
-                //if this setting is on, add the folder to the list of Folder string array
+                //String array used to store folders
+                var folders: [String] = [atPath]
+                
+                //Move through all the items in the folder
+                while let relativePath = pathEnumerator?.nextObject() as? String
+                    {
+                        //Set curremt path of the item
+                        let subItemAtPath = URL(fileURLWithPath: atPath).appendingPathComponent(relativePath).path
+                        //Set new path for the item
+                        let subItemToPath = URL(fileURLWithPath: toPath).appendingPathComponent(relativePath).path
+                        
+                        //Use isDir fuction to see if the object is a directory or file
+                        //If its a directory
+                        if isDir(atPath: subItemAtPath)
+                            {
+                                //if this setting is on, add the folder to the list of Folder string array
+                                if deleteEmptyFolders!
+                                    {
+                                        //Add path to array
+                                        folders.append(subItemAtPath)
+                                    }
+                                
+                                //If this folder does not exist
+                                if !isDir(atPath: subItemToPath)
+                                    {
+                                        do
+                                            {
+                                                //Create folder
+                                                try fileManager.createDirectory(atPath: subItemToPath, withIntermediateDirectories: true, attributes: nil)
+                                                print("FolderMerger: directory created: \(subItemToPath)")
+                                            }
+                                        catch let error
+                                            {
+                                                //Catch folder creation error
+                                                print("ERROR FolderMerger: \(error.localizedDescription)" )
+                                            }
+                                        
+                                    }
+                                else
+                                    {
+                                        //Folder already exists, no need to create new folder
+                                        print("FolderMerger: directory \(subItemToPath) already exists" )
+                                    }
+                            }
+                            
+                        //If its a file
+                        else
+                            {
+                                //TODO: Explore different options to keep source/keep destination. Currently errors happen when deleting the old files after moving.
+                                //If file already exists in path, delete it to move in the new version
+                                if isFile(atPath:subItemToPath) && conflictResolution == .keepSource
+                                    {
+                                        do
+                                            {
+                                                //Delete thie file
+                                                try fileManager.removeItem(atPath: subItemToPath)
+                                                print("FolderMerger: file deleted: \(subItemToPath)" )
+                                            }
+                                        catch let error
+                                            {
+                                                //Catch file deletion error
+                                                print("ERROR FolderMerger: \(error.localizedDescription)")
+                                            }
+                                    }
+                                
+                                //Move new version of file over to path
+                                do
+                                    {
+                                        //TODO: Use if statement to determine whether the item should be moved or copied.
+                                        // Maybe instead of using the moveItem command, use the copy item and then delete old file
+                                        
+                                        //Move the file
+                                        try fileManager.moveItem(atPath: subItemAtPath, toPath: subItemToPath)
+                                        print("FolderMerger: file moved from \(subItemAtPath) to \(subItemToPath)")
+                                    }
+                                catch let error
+                                    {
+                                        //Catch file move error
+                                        print("ERROR FolderMerger: \(error.localizedDescription)")
+                                    }
+                            }
+                    }
+                
+                //If this setting is on, will delete all folders in the Folders string array
                 if deleteEmptyFolders!
                 {
-                    //Add path to array
-                    folders.append(subItemAtPath)
-                }
-                
-                //If this folder does not exist
-                if !isDir(atPath: subItemToPath)
-                {
-                    do
+                    //Sort the array of folders
+                    folders.sort(by: { (path1, path2) -> Bool in
+                        return path1.characters.split(separator: "/").count < path2.characters.split(separator: "/").count
+                    })
+                    while let folderPath = folders.popLast()
                     {
-                        //Create folder
-                        try fileManager.createDirectory(atPath: subItemToPath, withIntermediateDirectories: true, attributes: nil)
-                        print("FolderMerger: directory created: \(subItemToPath)")
-                        
+                        //If the folder is empty
+                        if isDirEmpty(atPath: folderPath)
+                        {
+                            do
+                            {
+                                //Delete the folder
+                                try fileManager.removeItem(atPath: folderPath)
+                                print("FoldersMerger: empty dir deleted: \(folderPath)")
+                            }
+                            catch
+                            {
+                                //Catch folder deletion error
+                                print("ERROR FolderMerger: \(error.localizedDescription)")
+                            }
+                        }
                     }
-                    catch let error
-                    {
-                        //Catch folder creation error
-                        print("ERROR FolderMerger: \(error.localizedDescription)" )
-                        
-                    }
-                }
-                else
-                {
-                    //Folder already exists, no need to create new folder
-                    print("FolderMerger: directory \(subItemToPath) already exists" )
-                    
                 }
             }
-            //If its a file
-            else
+        
+        //Function used to determine whether the path is a directory
+        private func isDir(atPath: String) -> Bool
+        {
+            //Declare variable that will hold the return value of fileExist check
+            var isDir: ObjCBool = false
+            //Check if file exists in path and if its a directory retun it to the isDir boolean variable
+            let exist = fileManager.fileExists(atPath: atPath, isDirectory: &isDir)
+            
+            return exist && isDir.boolValue
+        }
+        
+        //Function used to determine whether the path is a file
+        private func isFile(atPath: String) -> Bool 
+        {
+            //Declare variable that will hold the return value of fileExist check
+            var isDir: ObjCBool = false
+                  //Check if file exists in path and if its a directory retun it to the isDir boolean variable
+            let exist = fileManager.fileExists(atPath: atPath, isDirectory: &isDir)
+            
+            return exist && !isDir.boolValue
+        }
+        
+        //Function used to determin is the direcoty is empy
+        private func isDirEmpty(atPath: String) -> Bool
+        {
+            do
             {
-                
-                //TODO: Explore different options to keep source/keep destination. Currently errors happen when deleting the old files after moving.
-                //If file already exists in path, delete it to move in the new version
-                if isFile(atPath:subItemToPath) && conflictResolution == .keepSource
-                {
-                    do
-                    {
-                        //Delete thie file
-                        try fileManager.removeItem(atPath: subItemToPath)
-                        print("FolderMerger: file deleted: \(subItemToPath)" )
-                        
-                    }
-                    catch let error
-                    {
-                        //Catch file deletion error
-                        print("ERROR FolderMerger: \(error.localizedDescription)")
-                        
-                    }
-                }
-                //Move new version of file over to path
-                do
-                {
-                    //TODO: Use if statement to determine whether the item should be moved or copied.
-                    // Maybe instead of using the moveItem command, use the copy item and then delete old file
-                    
-                    //Move the file
-                    try fileManager.moveItem(atPath: subItemAtPath, toPath: subItemToPath)
-                    print("FolderMerger: file moved from \(subItemAtPath) to \(subItemToPath)")
-                    
-                }
-                catch let error
-                {
-                    //Catch file move error
-                    print("ERROR FolderMerger: \(error.localizedDescription)")
-                    
-                }
+                //Return the number of items in direcory
+                return try fileManager.contentsOfDirectory(atPath: atPath).count == 0
             }
-        }
-        
-        //If this setting is on, will delete all folders in the Folders string array
-        if deleteEmptyFolders!
-        {
-            //Sort the array of folders
-            folders.sort(by: { (path1, path2) -> Bool in
-                return path1.characters.split(separator: "/").count < path2.characters.split(separator: "/").count
-            })
-            while let folderPath = folders.popLast()
+            catch _
             {
-                //If the folder is empty
-                if isDirEmpty(atPath: folderPath)
-                {
-                    do
-                    {
-                        //Delete the folder
-                        try fileManager.removeItem(atPath: folderPath)
-                        print("FoldersMerger: empty dir deleted: \(folderPath)")
-                    }
-                    catch
-                    {
-                        //Catch folder deletion error
-                        print("ERROR FolderMerger: \(error.localizedDescription)")
-                    }
-                }
+                return false
             }
         }
     }
-    
-    
-    //Function used to determine whether the path is a directory
-    private func isDir(atPath: String) -> Bool
-    {
-        //Declare variable that will hold the return value of fileExist check
-        var isDir: ObjCBool = false
-        //Check if file exists in path and if its a directory retun it to the isDir boolean variable
-        let exist = fileManager.fileExists(atPath: atPath, isDirectory: &isDir)
-        
-        return exist && isDir.boolValue
-    }
-    
-    //Function used to determine whether the path is a file
-    private func isFile(atPath: String) -> Bool 
-    {
-        //Declare variable that will hold the return value of fileExist check
-        var isDir: ObjCBool = false
-              //Check if file exists in path and if its a directory retun it to the isDir boolean variable
-        let exist = fileManager.fileExists(atPath: atPath, isDirectory: &isDir)
-        
-        return exist && !isDir.boolValue
-    }
-    
-    //Function used to determin is the direcoty is empy
-    private func isDirEmpty(atPath: String) -> Bool
-    {
-        do
-        {
-            //Return the number of items in direcory
-            return try fileManager.contentsOfDirectory(atPath: atPath).count == 0
-        }
-        catch _
-        {
-            return false
-        }
-    }
-}
--- PRIMARY_Backup/ViewController.swift
+++ PRIMARY_Backup/ViewController.swift
@@ -4,161 +4,162 @@
 //
 //  Created by David Guerrero on 3/2/17.
 //  Copyright © 2017 Primary Color. All rights reserved.
-//  Source code on GitHub repository
+//  
 
 import Cocoa
 import Foundation
 
 class ViewController: NSViewController
-{
-
-    @IBOutlet weak var copyFrom: NSTextField!
-    @IBOutlet weak var copyTo: NSTextField!
-    @IBOutlet weak var copyFromPath: NSPathControl!
-    @IBOutlet weak var copyToPath: NSPathControl!
-    @IBOutlet weak var startButton: NSButton!
-    
-    var fromURL:URL!
-    var toURL:URL!
-    
-    override func viewDidLoad()
     {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        @IBOutlet weak var copyFrom: NSTextField!
+        @IBOutlet weak var copyTo: NSTextField!
+        @IBOutlet weak var copyFromPath: NSPathControl!
+        @IBOutlet weak var copyToPath: NSPathControl!
+        @IBOutlet weak var startButton: NSButton!
+        @IBOutlet weak var testButton: NSButton!
+
         
-    }
-
-    override var representedObject: Any?
-    {
-        didSet
-        {
-        // Update the view, if already loaded.
-        }
-    }
-
-    @IBAction func startButtonPressed(_ sender: Any)
-    {
-
-        //If the copyFrom path is not empty
-        if copyFromPath.url  != nil
-        {
+        var fromURL:URL!
+        var toURL:URL!
         
-            //If the copyTo path is not empyy
-            if  copyToPath.url != nil
+        override func viewDidLoad()
             {
-                //Assign NSPathControl values to the URL variables
-                fromURL = copyFromPath.url
-                toURL = copyToPath.url
-                
-                //Show the fromURL on the copyFrom box for debugging
-                copyFrom.stringValue = fromURL.absoluteString
+                super.viewDidLoad()
+
+                // Do any additional setup after loading the view.
                 
             }
-            
-        }
-        
-        //Declare and initialize the custom Folder_Merger class
-        let merger = Folder_Merger(actionType: .copy, conflictResolution: .keepSource)
-    
-        //Merge the folders using the fromURl and toURL variables
-        merger.merge(atPath: fromURL.path, toPath: toURL.path)
-        
-        
-    }
-    
-    //Displays an Alert box on screen for the user to dismiss
-    func dialogAlert(alert: String, text: String) -> Bool
-    {
-        //Initialize an NSAlert object
-        let myPopup: NSAlert = NSAlert()
-        
-        //Set the alert message to the alert variable
-        myPopup.messageText = alert
-        
-        //Set the informativeText to the body text variable
-        myPopup.informativeText = text
-        
-        //Set alert style
-        myPopup.alertStyle = NSAlertStyle.critical
-        
-        //Dialog will only have 1 button to dismiss
-        myPopup.addButton(withTitle: "Ok")
-        
-        //Return whether the button was clicked to dismiss the dialog
-        return myPopup.runModal() == NSAlertFirstButtonReturn
-        
-    }
-    
-      //Displays an question with a YES/NO button to dismiss
-    func dialogOKCancel(question: String, text: String) -> Bool
-    {
-        //Initialize an NSAlert object
-        let myPopup: NSAlert = NSAlert()
-        
-         //Set the alert message to a question variable
-        myPopup.messageText = question
-        
-        //Set the informativeText to the body text variable
-        myPopup.informativeText = text
-        
-        //Set alert style
-        myPopup.alertStyle = NSAlertStyle.warning
-        
-        //Dialog will have two buttons
-        myPopup.addButton(withTitle: "Yes")
-        myPopup.addButton(withTitle: "No")
-        
-        //Return whather the first button was clicked to dismiss the dialog
-        return myPopup.runModal() == NSAlertFirstButtonReturn
-    }
-    
-    //Function with a PopUp panel to choose a folder, will return a URL variable
-    func popUpSelect() -> URL
-    {
-    
-        //Initialize a new NSOpenPanel object
-        let myBrowser: NSOpenPanel = NSOpenPanel()
-        
-        //Panel browser cannot choose files, but can directories
-        myBrowser.canChooseFiles = false
-        myBrowser.canChooseDirectories = true
-        
-        //Show the panel rowser
-        myBrowser.runModal()
-        
-        //Save the result to a URL variable
-        let result = myBrowser.url
-        
-        //Return the variable
-        return result!
 
+        override var representedObject: Any?
+            {
+                didSet
+                    {
+                    // Update the view, if already loaded.
+                    }
+            }
+
+        @IBAction func startButtonPressed(_ sender: Any)
+            {
+
+                //If the copyFrom path is not empty
+                if copyFromPath.url  != nil
+                    {
+                        //If the copyTo path is not empyy
+                        if  copyToPath.url != nil
+                            {
+                                //Assign NSPathControl values to the URL variables
+                                fromURL = copyFromPath.url
+                                toURL = copyToPath.url
+                                
+                                //Show the fromURL on the copyFrom box for debugging
+                                copyFrom.stringValue = fromURL.absoluteString
+                                
+                            }
+                    }
+                
+                //Declare and initialize the custom Folder_Merger class
+                let merger = Folder_Merger(actionType: .copy, conflictResolution: .keepSource)
+            
+                //Merge the folders using the fromURl and toURL variables
+                merger.merge(atPath: fromURL.path, toPath: toURL.path)
+            }
+    
+        //Button will be used to test custom functions
+        @IBAction func testButtonPressed(_ sender: Any)
+            {
+                
+                let textToCut = copyFrom.stringValue
+                
+               copyTo.stringValue =  textToCut.right(Integer: 5)!
+                
+                
+            }
+        
+        //Displays an Alert box on screen for the user to dismiss
+        func dialogAlert(alert: String, text: String) -> Bool
+            {
+                //Initialize an NSAlert object
+                let myPopup: NSAlert = NSAlert()
+                
+                //Set the alert message to the alert variable
+                myPopup.messageText = alert
+                
+                //Set the informativeText to the body text variable
+                myPopup.informativeText = text
+                
+                //Set alert style
+                myPopup.alertStyle = NSAlertStyle.critical
+                
+                //Dialog will only have 1 button to dismiss
+                myPopup.addButton(withTitle: "Ok")
+                
+                //Return whether the button was clicked to dismiss the dialog
+                return myPopup.runModal() == NSAlertFirstButtonReturn
+                
+            }
+        
+          //Displays an question with a YES/NO button to dismiss
+        func dialogOKCancel(question: String, text: String) -> Bool
+            {
+                //Initialize an NSAlert object
+                let myPopup: NSAlert = NSAlert()
+                
+                 //Set the alert message to a question variable
+                myPopup.messageText = question
+                
+                //Set the informativeText to the body text variable
+                myPopup.informativeText = text
+                
+                //Set alert style
+                myPopup.alertStyle = NSAlertStyle.warning
+                
+                //Dialog will have two buttons
+                myPopup.addButton(withTitle: "Yes")
+                myPopup.addButton(withTitle: "No")
+                
+                //Return whather the first button was clicked to dismiss the dialog
+                return myPopup.runModal() == NSAlertFirstButtonReturn
+            }
+        
+        //Function with a PopUp panel to choose a folder, will return a URL variable
+        func popUpSelect() -> URL
+            {
+                //Initialize a new NSOpenPanel object
+                let myBrowser: NSOpenPanel = NSOpenPanel()
+                
+                //Panel browser cannot choose files, but can directories
+                myBrowser.canChooseFiles = false
+                myBrowser.canChooseDirectories = true
+                
+                //Show the panel Browser
+                myBrowser.runModal()
+                
+                //Save the result to a URL variable
+                let result = myBrowser.url
+                
+                //Return the variable
+                return result!
+            }
+        
+        //Simple function that wll move files from one URL to another using the FileManager
+        func MoveFiles(moveFrom: URL, moveTo: URL)
+            {
+                
+                //Begin Error catching
+                do
+                    {
+                       //Initialize a new FileManager object
+                        let myfileManager: FileManager = FileManager()
+                        
+                        //Catch error on this line, Try to move files from one URL to another
+                        try myfileManager.copyItem(at: moveFrom, to: moveTo)
+                    }
+                catch
+                    {
+                    //Let user know file transfer failed via button that was pressed.
+                    startButton.title = "Failed"
+                    }
+            }
     }
-    
-    //Simple function that wll move files from one URL to another using the FileManager
-    func MoveFiles(moveFrom: URL, moveTo: URL)
-    {
-        
-        //Begin Error catching
-        do
-        {
-           //Initialize a new FileManager object
-            let myfileManager: FileManager = FileManager()
-            
-            //Catch error on this line, Try to move files from one URL to another
-            try myfileManager.copyItem(at: moveFrom, to: moveTo)
-            
-            
-        }
-        catch
-        {
-        
-        //Let user know file transfer failed via button that was pressed.
-        startButton.title = "Failed"
-        
-        }
-    
-    }
-    
-}
 
